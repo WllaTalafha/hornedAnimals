b06a5afe89ad88d2398590bd89d968642f001279
Wlla Talafha <wlla.issa396@gmail.com>
2022-08-04 02:59:23 +0300

    filter is done

--- src/components/main.js
+++ src/components/main.js
@@ -16,16 +16,14 @@ class Main extends React.Component {
 
 
 
-    filterTheBeast = (e) => {
-        const hornedNum = parseInt(e.target.value);
-    
-        if (hornedNum) {
-            let filterTemp = Data.filter(item => item.horns === hornedNum);
-            this.setState({ filter: filterTemp })
+    filterBeastByHornNumber = (e)=>{
+        const hornNumber = parseInt(e.target.value);
+        let filteredData = Data;
+        if(hornNumber){
+          filteredData= filteredData.filter(item => item.horns === hornNumber); 
         }
-            
-
-    }
+        this.setState({filter : filteredData})
+        }
 
     render() {
         return (
@@ -33,7 +31,7 @@ class Main extends React.Component {
 
                 <Form.Group className="mb-3">
                     <Form.Label htmlFor="disabledSelect">Disabled select menu</Form.Label>
-                    <Form.Select id="disabledSelect" onChange={this.filterTheBeast} >
+                    <Form.Select id="disabledSelect" onChange={this.filterBeastByHornNumber} >
                         <option value=''>All</option>
                         <option value='1'>One Beast</option>
                         <option value='2'>Two Beast</option>
